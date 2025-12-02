4ee006c9474ff5b60c2dd83ecb9ddd48bb5806b4
Aliyah Miranti Daleya <254107060060@student.polinema.ac.id>
2025-12-03 06:53:08 +0700

    Modifikasi Percobaan 2 + Pertanyaan

--- Kafe05.java
+++ Kafe05.java
@@ -1,13 +1,22 @@
 public class Kafe05 {
 
-    public static void Menu(String namaPelanggan, boolean isMember) {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
 
-        System.out.println("===== MENU RESTO KAFE =====");
+        // Cek kode promo
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kode promo valid! Anda mendapatkan diskon 50%!");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kode promo valid! Anda mendapatkan diskon 30%!");
+        } else {
+            System.out.println("Kode promo invalid!");
+        }
+
+        System.out.println("\n===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam – Rp 15,000");
         System.out.println("2. Cappuccino – Rp 20,000");
         System.out.println("3. Latte – Rp 22,000");
@@ -19,14 +28,25 @@ public class Kafe05 {
     }
 
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Budi", true, "DISKON30");
     }
 }
 
 
-// Jawaban Pertanyaan
+
+// Jawaban Pertanyaan Percobaan 1
 
 // 1. Tidak. Fungsi tanpa parameter tidak harus bertipe `void`; bisa mengembalikan nilai (misal `int`, `String`, dll).
 // 2. Bisa. Semua `System.out.println()` ditulis langsung di dalam `main` tanpa membuat fungsi `Menu()`.
 // 3. Kode lebih rapi, mudah dibaca, bisa dipakai ulang, dan mudah diperbarui.
 // 4. Program mulai di `main` → memanggil `Menu()` → isi fungsi dijalankan → menu tampil → kembali ke `main`.
+
+
+// Jawaban Pertanyaan Percobaan 2
+
+// 1. Fungsi parameter → mengirim data/nilai dari luar fungsi agar fungsi bisa bekerja sesuai kebutuhan.
+// 2. namaPelanggan & isMember → namaPelanggan untuk menampilkan sapaan, isMember untuk menentukan apakah dapat diskon atau tidak.
+// 3. Parameter ≠ variabel → parameter menerima input dari luar fungsi, variabel menyimpan data selama program berjalan.
+// 4. isMember → jika true tampil pesan diskon, jika false tidak tampil.
+// 5. Jika fungsi dipanggil tanpa parameter → error, karena fungsi membutuhkan input.
+// 6. Parameter kodePromo digunakan untuk mengecek kode promosi dan menentukan besar diskon.
