1755f3fdd6d922d98cfc5ebf86382ff509f34444
Aliyah Miranti Daleya <254107060060@student.polinema.ac.id>
2025-12-03 07:32:51 +0700

    Modifikasi Tugas 3

--- RekapPenjualanCafe05.java
+++ RekapPenjualanCafe05.java
@@ -2,29 +2,44 @@ import java.util.Scanner;
 
 public class RekapPenjualanCafe05 {
 
-    String[] menu = {"Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan"};
-    int[][] data = new int[5][7];
+    String[] menu;
+    int[][] data;
+    int jmlMenu, jmlHari;
 
     public void inputPenjualan() {
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
-            System.out.println("\nInput penjualan menu: " + menu[i]);
-            for (int j = 0; j < 7; j++) {
+
+        System.out.print("Masukkan jumlah menu: ");
+        jmlMenu = sc.nextInt();
+        menu = new String[jmlMenu];
+
+        System.out.print("Masukkan jumlah hari penjualan: ");
+        jmlHari = sc.nextInt();
+        data = new int[jmlMenu][jmlHari];
+
+        sc.nextLine(); 
+
+        for (int i = 0; i < jmlMenu; i++) {
+            System.out.print("\nNama menu ke-" + (i + 1) + ": ");
+            menu[i] = sc.nextLine();
+
+            for (int j = 0; j < jmlHari; j++) {
                 System.out.print("Hari ke-" + (j + 1) + " : ");
                 data[i][j] = sc.nextInt();
             }
+            sc.nextLine();
         }
     }
 
     public void tampilData() {
         System.out.println("\n===== Tabel Penjualan =====");
         System.out.print("Menu\t\t");
-        for (int h = 1; h <= 7; h++) System.out.print("H" + h + "\t");
+        for (int h = 1; h <= jmlHari; h++) System.out.print("H" + h + "\t");
         System.out.println();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < jmlMenu; i++) {
             System.out.print(menu[i] + "\t");
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < jmlHari; j++) {
                 System.out.print(data[i][j] + "\t");
             }
             System.out.println();
@@ -34,9 +49,9 @@ public class RekapPenjualanCafe05 {
     public void tertinggi() {
         int max = 0;
         String menuMax = "";
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < jmlMenu; i++) {
             int total = 0;
-            for (int j = 0; j < 7; j++) total += data[i][j];
+            for (int j = 0; j < jmlHari; j++) total += data[i][j];
 
             if (total > max) {
                 max = total;
@@ -48,10 +63,10 @@ public class RekapPenjualanCafe05 {
 
     public void rataMenu() {
         System.out.println("\n===== Rata-rata Penjualan =====");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < jmlMenu; i++) {
             int total = 0;
-            for (int j = 0; j < 7; j++) total += data[i][j];
-            System.out.println(menu[i] + " : " + (total / 7.0));
+            for (int j = 0; j < jmlHari; j++) total += data[i][j];
+            System.out.println(menu[i] + " : " + (total / (double) jmlHari));
         }
     }
 
