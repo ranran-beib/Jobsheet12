8e6cf6bc4d2d09d37aa759646c45539563191677
Aliyah Miranti Daleya <254107060060@student.polinema.ac.id>
2025-12-03 07:27:05 +0700

    Tugas 1

---
+++ Kubus05.java
@@ -0,0 +1,12 @@
+public class Kubus05 {
+
+    // Fungsi menghitung volume kubus
+    public double volume(double sisi) {
+        return sisi * sisi * sisi;
+    }
+
+    // Fungsi menghitung luas permukaan kubus
+    public double luasPermukaan(double sisi) {
+        return 6 * sisi * sisi;
+    }
+}
