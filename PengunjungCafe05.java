dcb167e1470c81a2662b8757246489914429695e
Aliyah Miranti Daleya <254107060060@student.polinema.ac.id>
2025-12-03 07:13:36 +0700

    Modifikasi Percobaan 4 + Pertanyaan

--- PengunjungCafe05.java
+++ PengunjungCafe05.java
@@ -5,9 +5,19 @@ public class PengunjungCafe05 {
         for(int i = 0; i < namaPengunjung.length; i++) {
             System.out.println("- " + namaPengunjung[i]);
         }
+        System.out.println(); 
     }
 
     public static void main(String[] args) {
         daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
     }
 }
+
+
+// Jawaban Pertanyaan Percobaan 4
+
+1. 'String' digunakan karena varargs bisa menerima argumen banyak atau sedikit tanpa batas.
+2. Tidak bisa, dua varargs dalam satu fungsi; hanya satu dan harus di bagian akhir parameter.
+3. Program tetap berjalan, varargs menjadi array kosong, output hanya header tanpa daftar nama.
